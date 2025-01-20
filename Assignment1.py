3166a1edef86d7ae96ebac2eb8cb8efc315981f7
RatnaHarika001 <136871576+RatnaHarika001@users.noreply.github.com>
2025-01-20 09:31:11 -0600

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
+class assignment1():
+    
+    def assignment1(self):
+        driver = webdriver.Chrome()
 
-driver.maximize_window()
+        driver.maximize_window()
 
-driver.get("https://freelance-learn-automation.vercel.app/login")
-driver.find_element(By.XPATH,"//button[text()='Sign in']").click()
-Errormessage = driver.find_element(By.XPATH,"//*[contains(text(),'Email')]").is_displayed()
-if Errormessage == True:
-    print("Pass")
-else:
-    print("Fail")
+        driver.get("https://freelance-learn-automation.vercel.app/login")
+        driver.find_element(By.XPATH,"//button[text()='Sign in']").click()
+        Errormessage = driver.find_element(By.XPATH,"//*[contains(text(),'Email')]").is_displayed()
+        self.assertTrue(Errormessage.isdisplayed)
